a044b653908713a4c4a320f7736fca56ac20d6b4
Misternosh <misternosh123@gmail.com>
2026-03-10 19:14:35 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(const LuhaidanApp());
+void main() => runApp(const MaterialApp(home: QuranPlayer(), debugShowCheckedModeBanner: false));
 
-class LuhaidanApp extends StatelessWidget {
-  const LuhaidanApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: QuranPlayer(),
-    );
-  }
-}
+class QuranPlayer extends StatelessWidget {
+  const QuranPlayer({super.key});
 
-class QuranPlayer extends StatefulWidget {
-  @override
-  _QuranPlayerState createState() => _QuranPlayerState();
-}
-
-class _QuranPlayerState extends State<QuranPlayer> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  final List<Map<String, String>> playlist = [
+  final List<Map<String, String>> playlist = const [
     {"title": "سورة الحجر", "file": "Surah_al_hejr.mp3"},
     {"title": "سورة الكهف", "file": "Surah_al_kahf.mp3"},
     {"title": "سورة المؤمنون", "file": "Surah_al_mominon.mp3"},
@@ -30,22 +15,20 @@ class _QuranPlayerState extends State<QuranPlayer> {
     {"title": "سورة يوسف", "file": "Surah_yusuf.mp3"},
   ];
 
-  void playAudio(String fileName) async {
-    await _audioPlayer.stop();
-    await _audioPlayer.play(AssetSource('audio/$fileName'));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("القارئ محمد اللحيدان"), backgroundColor: Colors.green),
+      appBar: AppBar(title: const Text("محمد اللحيدان"), backgroundColor: Colors.green),
       body: ListView.builder(
         itemCount: playlist.length,
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(playlist[index]['title']!),
             trailing: const Icon(Icons.play_arrow),
-            onTap: () => playAudio(playlist[index]['file']!),
+            onTap: () async {
+              final player = AudioPlayer();
+              await player.play(AssetSource(playlist[index]['file']!));
+            },
           );
         },
       ),
